78db24f1ab4efad0389f95c673fd2640b971e12b
Charit_Patel <charit15499@gmail.com>
2023-02-17 04:49:47 -0500

    Product delete, Category CRUD operations api

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,20 @@
 const Category = require('../models/category')
 const errorHandler = require('../helpers/dbErrorHandler')
 
+
+exports.categoryById = (request, response, next ,id) => {
+    Category.findById(id, (error, category) => {
+        if(error || !category) {
+            return response.status(400).json({
+                error: "Category does not exist"
+            })
+        }
+        request.category = category
+        next()
+    })
+}
+
+
 exports.create = (request, response) => {
     const category = new Category(request.body)
     category.save((error, data) => {
@@ -12,5 +26,51 @@ exports.create = (request, response) => {
         response.json({
             data
         })
+    })
+}
+
+
+exports.read = (request, response) => {
+    return response.json(request.category)
+}
+
+
+exports.update = (request, response) => {
+    const category = request.category
+    category.name = request.body.name
+    category.save((error, data) => {
+        if(error) {
+            return response.status(400).json({
+                error: errorHandler(error)
+            })
+        }
+        return response.json(data)        
+    })
+}
+
+
+exports.remove = (request, response) => {
+    const category = request.category
+    category.remove((error, data) => {
+        if(error){
+            return response.status(400).json({
+                error: errorHandler(error)
+            })
+        }
+        return response.json({
+            message: "Category deleted"
+        })
+    })
+}
+
+
+exports.list = (request, response) => {
+    Category.find((error, data) => {
+        if(error){
+            return response.status(400).json({
+                error: errorHandler(error)
+            })
+        }
+        return response.json(data)
     })
 }
